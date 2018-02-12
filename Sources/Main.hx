8726a514c081332bd538514b8b2b0726ec5bb495
Robert Konrad <git2016@robdangero.us>
2018-02-12 13:18:17 +0100

    Update Bullet

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -21,36 +21,36 @@ class Main {
 	static var fallRigidBody: BtRigidBody;
 
 	static function initPhysics(): Void {
-		var collisionConfiguration = new BtDefaultCollisionConfiguration();
+		var collisionConfiguration = BtDefaultCollisionConfiguration.create();
 		var dispatcher = new BtCollisionDispatcher(collisionConfiguration);
 		var broadphase = new BtDbvtBroadphase();
 		var solver = new BtSequentialImpulseConstraintSolver();
 		dynamicsWorld = new BtDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
 
-		var groundShape = BtStaticPlaneShape.create(BtVector3.create(0, 1, 0).value, 1);
+		var groundShape = BtStaticPlaneShape.create(BtVector3.create(0, 1, 0), 1);
 		var groundTransform = BtTransform.create();
-		groundTransform.value.setIdentity();
-		groundTransform.value.setOrigin(BtVector3.create(0, -1, 0).value);
+		groundTransform.setIdentity();
+		groundTransform.setOrigin(BtVector3.create(0, -1, 0));
 		var centerOfMassOffsetTransform = BtTransform.create();
-		centerOfMassOffsetTransform.value.setIdentity();
-		var groundMotionState = BtDefaultMotionState.create(groundTransform.value, centerOfMassOffsetTransform.value);
+		centerOfMassOffsetTransform.setIdentity();
+		var groundMotionState = BtDefaultMotionState.create(groundTransform, centerOfMassOffsetTransform);
 
-		var groundRigidBodyCI = BtRigidBodyConstructionInfo.create(0.01, groundMotionState, groundShape, BtVector3.create(0, 0, 0).value);
-		var groundRigidBody = BtRigidBody.create(groundRigidBodyCI.value);
+		var groundRigidBodyCI = BtRigidBodyConstructionInfo.create(0.01, groundMotionState, groundShape, BtVector3.create(0, 0, 0));
+		var groundRigidBody = BtRigidBody.create(groundRigidBodyCI);
 		dynamicsWorld.addRigidBody(groundRigidBody);
 
 		var fallShape = BtSphereShape.create(1);
 		var fallTransform = BtTransform.create();
-		fallTransform.value.setIdentity();
-		fallTransform.value.setOrigin(BtVector3.create(0, 50, 0).value);
+		fallTransform.setIdentity();
+		fallTransform.setOrigin(BtVector3.create(0, 50, 0));
 		var centerOfMassOffsetFallTransform = BtTransform.create();
-		centerOfMassOffsetFallTransform.value.setIdentity();
-		var fallMotionState = BtDefaultMotionState.create(fallTransform.value, centerOfMassOffsetFallTransform.value);
+		centerOfMassOffsetFallTransform.setIdentity();
+		var fallMotionState = BtDefaultMotionState.create(fallTransform, centerOfMassOffsetFallTransform);
 
 		var fallInertia = BtVector3.create(0, 0, 0);
-		fallShape.value.calculateLocalInertia(1, fallInertia.value);
-		var fallRigidBodyCI = BtRigidBodyConstructionInfo.create(1, fallMotionState, fallShape, fallInertia.value);
-		fallRigidBody = BtRigidBody.create(fallRigidBodyCI.value);
+		fallShape.calculateLocalInertia(1, fallInertia);
+		var fallRigidBodyCI = BtRigidBodyConstructionInfo.create(1, fallMotionState, fallShape, fallInertia);
+		fallRigidBody = BtRigidBody.create(fallRigidBodyCI);
 		dynamicsWorld.addRigidBody(fallRigidBody);
 	}
 
@@ -58,8 +58,8 @@ class Main {
 		dynamicsWorld.stepSimulation(1 / 60);
 	
 		var trans = BtTransform.create();
-		var m = fallRigidBody.value.getMotionState();
-		m.getWorldTransform(trans.value);
+		var m = fallRigidBody.getMotionState();
+		m.getWorldTransform(trans);
 		trace(trans.getOrigin().y());
 	}
 
